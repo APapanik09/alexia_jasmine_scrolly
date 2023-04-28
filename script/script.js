54f34e6306d14be06e4c8ba06adab5bc9d8bae93
APapanik09 <89647801+APapanik09@users.noreply.github.com>
2023-04-28 03:19:59 -0400

    p.2.2b animation de base
    yippee!

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 const cta = document.querySelector(".cta");
 const body = document.querySelector("body");
 let minuteur;
@@ -18,4 +20,41 @@ gsap.from('.cta',{
 
 })
 
- 
+
+//repeat mis pour montrer l'animation avant l'étape du scrolltrigger
+
+ gsap
+ .timeline()
+ .from(".spritefall", { bottom: '10vh', duration: 3, ease: 'in', repeat: -1 })
+ .from(".boucle", { top:"20vh", ease:'in', repeat: -1, duration: 3, opacity: '100%'}, 0.25)
+ .from(".collier", { top:"33vh",ease:'in', repeat: -1, duration: 3, opacity: '100%'}, 0.25);
+
+ gsap
+ .timeline()
+ .from(".question_1", {y: '20px', yoyo:true, ease: 'in', repeat: -1 })
+ .from(".question_2", { rotation:"360", ease:'in', repeat: -1, duration: 3}, 0.25);
+
+ gsap.from('.vuedessus',{
+    repeat: -1,
+    top: '5vh',
+    rotate: '90',
+    left: '10vw',
+    duration: 5,
+    ease: 'in-out'
+    
+
+})
+
+gsap
+.timeline()
+.from(".spritemarche", {left: '-90vw', ease: 'out', repeat: -1, duration: 4})
+.from(".chatsaute", { rotation:"-10", ease:'out', repeat: -1, duration: 2, right:'5vw', bottom: '55vh', opacity: '0'}, 3);
+
+gsap.from('.etoileminou',{
+    repeat: -1,
+    top: '-10vh',
+    duration: 3,
+    ease: 'out',
+    opacity: '0'
+
+})
